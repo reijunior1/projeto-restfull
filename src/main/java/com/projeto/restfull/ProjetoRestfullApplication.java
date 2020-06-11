52e5ab832f3bc383ba796e2a4d30ef2d7ea812dc
reijunior1 <reijunior1@hotmail.com>
2020-06-10 21:04:52 -0300

    iniciando aplicação REST com SPRING-MVC

---
+++ src/main/java/com/projeto/restfull/ProjetoRestfullApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestfullApplication.class, args);
+	}
+
+}
